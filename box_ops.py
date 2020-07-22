6aa2759a27cbfe39042a0c31c02efb6621d0b43a
PlasmaDuck <66852667+PlasmaDuck@users.noreply.github.com>
2020-07-22 15:19:22 -0700

    Update box_ops.py

--- box_ops.py
+++ box_ops.py
@@ -11,6 +11,13 @@
 # "Distance-IoU Loss: Faster and Better Learning for Bounding Box Regression"
 # credit Zhaohui Zheng, Ping Wang, Wei Liu, Jinze Li, Rongguang Ye, Dongwei Ren
 
+# Instructions for use - all that is needed is a one line change to point it to the new loss function:
+# 1 - Open detr/models/detr.py
+# 2 - Look for the function loss_boxes, (approx line 159 as of this writing)
+# 3 - comment out this line:          loss_giou = 1 - torch.diag(box_ops.generalized_box_iou(
+# 4 - insert this line in its place:  loss_giou = 1 - torch.diag(box_ops.complete_box_iou(
+# 5 - make sure it looks syntactically correct, then save
+
 """
 Utilities for bounding box manipulation and GIoU.
 """
